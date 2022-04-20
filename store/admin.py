24ce06c94f0998811ee1419baf43988b719723e7
devkenTheGeek <tech.wiggles@gmail.com>
2022-04-20 12:43:41 +0300

    admin site

--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from store.models import *
+
+admin.site.register(Order)
+admin.site.register(Products)
+admin.site.register(Category)
+admin.site.register(Customer)
+
+
--- store/views.py
+++ store/views.py
@@ -1,10 +1,30 @@
 from django.shortcuts import render
 
-
 # Create your views here.
+from store.models import Category, Products
+
+
+#
+# def store(request):
+#     context = {}
+#     return render(request, 'store/store.html', context)
+
 def store(request):
-    context = {}
-    return render(request, 'store/store.html', context)
+    cart = request.session.get('cart')
+    if not cart:
+        request.session['cart'] = {}
+    products = None
+    categories = Category.get_all_categories()
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Products.get_all_products_by_categoryid(categoryID)
+    else:
+        products = Products.get_all_products()
+
+    data = {'products': products, 'categories': categories}
+
+    # print('you are : ', request.session.get('email'))
+    return render(request, 'store/store.html', data)
 
 
 def cart(request):
